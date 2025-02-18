e57e6294e48d0203f1ac8d8952a28ffc0b15e774
cristianorregod <criferorgdk@gmail.com>
2025-02-17 21:12:21 -0500

    Fix: Modifica validacion del token

--- middlewares/jwt_bearer.py
+++ middlewares/jwt_bearer.py
@@ -6,6 +6,9 @@ from fastapi import Request, HTTPException
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
         auth = await super().__call__(request)
+        print("credentials", auth.credentials)
+        print("iniciando validacion de token")
         is_valid = validate_token(auth.credentials)
-        if not is_valid:
-            raise HTTPException(status_code=403, detail="Invalid token")
+        print("is_valid", is_valid)
+        if is_valid.get('error'):
+            raise HTTPException(status_code=403, detail=is_valid['error'])
--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import os
-from jwt import encode, decode
+from jwt import encode, decode, ExpiredSignatureError, InvalidTokenError
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
@@ -15,5 +15,14 @@ def create_token(data: dict):
 
 
 def validate_token(token: str):
-    data: dict = decode(token, key=JWT_SECRET_KEY, algorithms=["HS256"])
-    return data
+    print("token recibido")
+    try:
+        data: dict = decode(token, key=JWT_SECRET_KEY, algorithms=["HS256"])
+        print("data decodificada", data)
+        return data
+    except ExpiredSignatureError:
+        print("token expirado")
+        return {"error": "token expired"}
+    except InvalidTokenError:
+        print("token invalido")
+        return {"error": "invalid token"}
